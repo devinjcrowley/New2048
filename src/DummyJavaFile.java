70269e3916ca4c9a57ee33845d116640874d52f9
Yale Fang <yalfan22@bergen.org>
2019-05-29 12:14:44 -0400

    yioijkl

--- src/DummyJavaFile.java
+++
@@ -1,5 +0,0 @@
-public class DummyJavaFile {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
--- src/DummyJavaYale.java
+++
@@ -1,4 +0,0 @@
-public class DummyJavaYale {
-    public static void main (String[] args) {
-    }
-}
