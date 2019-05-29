086cedbcdcaa3098276c2bbd213c6f9f1327adc8
Devin Crowley <devcro22@bergen.org>
2019-05-29 12:16:03 -0400

    First creation of 2048 file. Starting code.

---
+++ src/Devin2048.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Devin2048 extends Application {
+
+    @Override
+    public void start(Stage ps) {
+
+    }
+}
