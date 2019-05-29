5f8dc6c02d42326bbcc1b05ec1a66d0373d31c68
Devin Crowley <devcro22@bergen.org>
2019-05-29 12:31:09 -0400

    First creation of 2048 file. Starting code.

--- src/Devin2048.java
+++ src/Devin2048.java
@@ -1,4 +1,11 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -6,6 +13,12 @@ public class Devin2048 extends Application {
 
     @Override
     public void start(Stage ps) {
+        BorderPane background = new BorderPane();
+        background.setStyle("-fx-background-color: #F2E2D2");
 
+        Scene scene = new Scene(background, 500, 500);
+        ps.setTitle("Devin 2048");
+        ps.setScene(scene);
+        ps.show();
     }
 }
