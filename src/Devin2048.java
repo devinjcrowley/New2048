357a1a9111b59f4661df850ea05c3688c567c66b
Devin Crowley <devcro22@bergen.org>
2019-05-30 12:14:40 -0400

    First creation of 2048 file. Starting code.

--- src/Devin2048.java
+++ src/Devin2048.java
@@ -1,10 +1,7 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -13,8 +10,33 @@ public class Devin2048 extends Application {
 
     @Override
     public void start(Stage ps) {
+
+        // Establishing the background of the game.
         BorderPane background = new BorderPane();
-        background.setStyle("-fx-background-color: #F2E2D2");
+        background.setStyle("-fx-background-color: #faf8ef");
+
+
+        //Setting up the top bar of the game.
+        HBox topBar = new HBox();
+        GridPane topBarSplit = new GridPane(); //For splitting the name and scores.
+
+        Pane name = new Pane(); //For "2048" at the top of the screen.
+        topBarSplit.add(name, 0, 0);
+
+        GridPane scoresSplit = new GridPane(); //Splitting scores.
+        topBarSplit.add(scoresSplit, 1, 0);
+
+        Pane score = new Pane();
+        Pane bestScore = new Pane();
+        scoresSplit.add(score, 0, 0);
+        scoresSplit.add(bestScore, 1, 0);
+
+        topBar.getChildren().add(topBarSplit);
+
+        background.setTop(topBar);
+
+        //Setting up the board of the game.
+        Pane gameBoarder = new Pane();
 
         Scene scene = new Scene(background, 500, 500);
         ps.setTitle("Devin 2048");
