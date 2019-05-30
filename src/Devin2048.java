d49c4cd30ac544afa237453facf43f3c0aa9de9d
Devin Crowley <devcro22@bergen.org>
2019-05-30 12:23:32 -0400

    Adding Top and Middle

--- src/Devin2048.java
+++ src/Devin2048.java
@@ -16,27 +16,39 @@ public class Devin2048 extends Application {
         background.setStyle("-fx-background-color: #faf8ef");
 
 
-        //Setting up the top bar of the game.
-        HBox topBar = new HBox();
-        GridPane topBarSplit = new GridPane(); //For splitting the name and scores.
-
-        Pane name = new Pane(); //For "2048" at the top of the screen.
-        topBarSplit.add(name, 0, 0);
-
-        GridPane scoresSplit = new GridPane(); //Splitting scores.
-        topBarSplit.add(scoresSplit, 1, 0);
-
-        Pane score = new Pane();
-        Pane bestScore = new Pane();
-        scoresSplit.add(score, 0, 0);
-        scoresSplit.add(bestScore, 1, 0);
-
-        topBar.getChildren().add(topBarSplit);
-
-        background.setTop(topBar);
+//        //Setting up the top bar of the game.
+//        HBox topBar = new HBox();
+//        GridPane topBarSplit = new GridPane(); //For splitting the name and scores.
+//
+//        Pane name = new Pane(); //For "2048" at the top of the screen.
+//        topBarSplit.add(name, 0, 0);
+//
+//        GridPane scoresSplit = new GridPane(); //Splitting scores.
+//        topBarSplit.add(scoresSplit, 1, 0);
+//
+//        Pane score = new Pane();
+//        Pane bestScore = new Pane();
+//        scoresSplit.add(score, 0, 0);
+//        scoresSplit.add(bestScore, 1, 0);
+//
+//        topBar.getChildren().add(topBarSplit);
+//
+//        background.setTop(topBar);
 
         //Setting up the board of the game.
-        Pane gameBoarder = new Pane();
+        BorderPane gameBoarder = new BorderPane();
+        gameBoarder.setStyle("-fx-background-color: #bbada0");
+
+        GridPane board = new GridPane();
+        board.setVgap(10);
+        board.setHgap(10);
+        board.setStyle("-fx-background-color: #cdc1b4");
+
+        
+
+        gameBoarder.setCenter(board);
+
+        background.getChildren().add(gameBoarder);
 
         Scene scene = new Scene(background, 500, 500);
         ps.setTitle("Devin 2048");
