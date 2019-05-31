d0e438ae0346ab9981dca8712a6dc24909efefc8
Yale Fang <yalfan22@bergen.org>
2019-05-31 11:28:15 -0400

    w

--- src/Yale2048.java
+++ src/Yale2048.java
@@ -7,27 +7,31 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class Yale2048 extends Application {
     public void start (Stage ps) {
-        HBox hb = new HBox();
-
         BorderPane background = new BorderPane();
 
         BorderPane gameBorder = new BorderPane();
+        gameBorder.setPrefSize(50, 100);
         gameBorder.setStyle("-fx-background-color: #bbada0");
 
         GridPane board = new GridPane();
+        board.setPrefSize(90, 90);
         board.setStyle("-fx-background-color: #cdc1b4");
 
         gameBorder.setCenter(board);
 
 
         // Top
-        GridPane scoreAndText = new GridPane();
+        HBox hb = new HBox();
+        hb.setSpacing(500);
+        hb.setAlignment(Pos.CENTER);
+        GridPane scoreAndBest = new GridPane();
         VBox scorePane = new VBox();
         VBox bestScorePane = new VBox();
 
@@ -43,9 +47,9 @@ public class Yale2048 extends Application {
         scoreText.setFont(Font.font("Calibri", 30));
         scoreText.setFill(Color.web("#ffffff"));
 
-        scoreAndText.add(scorePane, 0, 0);
-        scoreAndText.add(bestScorePane, 1, 0);
-        scoreAndText.setHgap(20);
+        scoreAndBest.add(scorePane, 0, 0);
+        scoreAndBest.add(bestScorePane, 1, 0);
+        scoreAndBest.setHgap(20);
 
         scorePane.setPrefHeight(50);
         scorePane.setPrefWidth(75);
@@ -69,11 +73,41 @@ public class Yale2048 extends Application {
         bestScorePane.setAlignment(Pos.CENTER);
         bestScorePane.setStyle("-fx-background-color: #bbada0");
 
-        hb.getChildren().addAll(text, scoreAndText);
-        hb.setSpacing(300);
-        background.setStyle("-fx-background-color: #F2E2D2");
+        hb.getChildren().addAll(text, scoreAndBest);
 
+        // Bottom
+        Pane i = new Pane();
+        i.prefHeightProperty().bind(board.heightProperty().add(60));
+        i.prefWidthProperty().bind(background.widthProperty());
+        background.setBottom(i);
+        //instruction header text:
+        Text insHead = new Text( "HOW TO PLAY: ");
+        insHead.setFont(Font.font ("Calibri", FontWeight.BOLD, 20));
+        insHead.setFill(Color.web("#776e65"));
+        insHead.setStroke(Color.web("#776e65"));
+        insHead.xProperty().bind(i.widthProperty().divide(2).subtract(200));
+        insHead.yProperty().bind(i.heightProperty().subtract(35));
+//instructions body text:
+        Text inst1 = new Text("Use your arrow keys to move the tiles.");
+        inst1.setFont(Font.font ("Calibri",15));
+        inst1.setFill(Color.web("#776e65"));
+        inst1.setStroke(Color.web("#776e65"));
+        inst1.xProperty().bind(insHead.xProperty().add(130));
+        inst1.yProperty().bind(i.heightProperty().subtract(35));
+        Text inst2 = new Text("When two tiles with the same " +
+                "number touch, they merge into one!");
+        inst2.setFont(Font.font ("Calibri",15));
+        inst2.setFill(Color.web("#776e65"));
+        inst2.setStroke(Color.web("#776e65"));
+        inst2.xProperty().bind(i.widthProperty().divide(2).subtract(200));
+        inst2.yProperty().bind(i.heightProperty().subtract(15));
+        i.getChildren().addAll(insHead, inst1, inst2);
 
+        // Sides
+        VBox side1 = new VBox();
+        VBox side2 = new VBox();
+        side1.setPrefWidth(50);
+        side2.setPrefWidth(50);
 
         // Center
 
@@ -82,8 +116,13 @@ public class Yale2048 extends Application {
 
         background.setCenter(gameBorder);
         background.setTop(hb);
+        background.setBottom(i);
+        background.setLeft(side1);
+        background.setRight(side2);
 
-        Scene scene = new Scene(background, 600, 400);
+
+        background.setStyle("-fx-background-color: #F2E2D2");
+        Scene scene = new Scene(background, 1000, 600);
         ps.setScene(scene);
         ps.show();
     }
