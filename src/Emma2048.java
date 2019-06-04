295c5717c4efa50eee0a45b685366588508eea17
emmillet <hellosunshinq@gmail.com>
2019-06-04 17:37:11 -0400

    h

--- src/Emma2048.java
+++ src/Emma2048.java
@@ -13,11 +13,20 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import static javafx.scene.input.KeyCode.LEFT;
+import static javafx.scene.input.KeyCode.RIGHT;
 import static javafx.scene.input.KeyEvent.KEY_PRESSED;
 
 
 public class Emma2048 extends Application {
     public void start (Stage ps) {
+        StackPane[][] a = {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+        };
+
         BorderPane background = new BorderPane();
 
         BorderPane gameBorder = new BorderPane();
@@ -150,19 +159,14 @@ public class Emma2048 extends Application {
 //            }
 //                });
 
-        int[][] a = {
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-        };
         //this is a variable to check whether or not the board is full, and below is a VERY long if loop for it
         int full = 0;
 
-        if(a[0][0]== 1 && a[0][1]== 1 && a[0][2]== 1 && a[0][3]== 1 && a[0][4]== 1 && a[1][0]== 1 && a[1][1]== 1
-                && a[1][2]== 1 && a[1][3]== 1 && a[1][4]== 1 && a[2][0]== 1 && a[2][1]== 1 && a[2][2]== 1
-                && a[2][3]== 1 && a[2][4]== 1 && a[3][0]== 1 && a[3][1]== 1 && a[3][2]== 1 && a[3][3]== 1
-                && a[3][4]== 1){
+        if(a[0][0]!=null && a[0][1]!=null && a[0][2]!=null && a[0][3]!=null && a[0][4]!=null &&
+                a[1][0]!=null && a[1][1]!=null && a[1][2]!=null && a[1][3]!=null && a[1][4]!=null
+                && a[2][0]!=null && a[2][1]!=null && a[2][2]!=null && a[2][3]!=null
+                && a[2][4]!=null && a[3][0]!=null && a[3][1]!=null && a[3][2]!=null && a[3][3]!=null
+                && a[3][4]!=null){
             full = 1;
         }
          for (int j = 0; j < 2; j++) {
@@ -186,88 +190,73 @@ public class Emma2048 extends Application {
                   r2.setAccessibleText("2");
                   s2.getChildren().addAll(r2, t2);
               }
-              int row = (int) (Math.random() * 4);
+             if (r2.getAccessibleText().equals("")) {
+                 r2.setFill(Color.web("#eee4da"));
+             } else if (r2.getAccessibleText().equals("2")) {
+                 r2.setFill(Color.web("#d6d4d3"));
+             } else if (r2.getAccessibleText().equals("4")) {
+                 r2.setFill(Color.web("#e6ceb3"));
+             } else if (r2.getAccessibleText().equals("8")) {
+                 r2.setFill(Color.web("#e8a67e"));
+             } else if (r2.getAccessibleText().equals("16")) {
+                 r2.setFill(Color.web("#e88a4b"));
+             } else if (r2.getAccessibleText().equals("32")) {
+                 r2.setFill(Color.web("#ff7a65"));
+             } else if (r2.getAccessibleText().equals("64")) {
+                 r2.setFill(Color.web("#eb4e2f"));
+             } else if (r2.getAccessibleText().equals("128")) {
+                 r2.setFill(Color.web("#f0df73"));
+             } else if (r2.getAccessibleText().equals("256")) {
+                 r2.setFill(Color.web("#f0df48"));
+             } else if (r2.getAccessibleText().equals("512")) {
+                 r2.setFill(Color.web("#f2cf09"));
+             } else if (r2.getAccessibleText().equals("1024")) {
+                 r2.setFill(Color.web("#ffee6b"));
+             } else if (r2.getAccessibleText().equals("2048")) {
+                 r2.setFill(Color.web("#fff587"));
+             }
+
+             int row = (int) (Math.random() * 4);
               int column = (int) (int) (Math.random() * 4);
-              a[row][column] = 1;
               board.add(s2, row, column);
-              if (r2.getAccessibleText().equals("")) {
-                  r2.setStyle(("-fx-background-color:#cdc1b4"));
-              } else if (r2.getAccessibleText().equals("2")) {
-                  r2.setStyle("-fx-background-color:#d6d4d3");
-              } else if (r2.getAccessibleText().equals("4")) {
-                  r2.setStyle("-fx-background-color:#e6ceb3");
-              } else if (r2.getAccessibleText().equals("8")) {
-                  r2.setStyle("-fx-background-color:#e8a67e");
-              } else if (r2.getAccessibleText().equals("16")) {
-                  r2.setStyle("-fx-background-color:#e88a4b");
-              } else if (r2.getAccessibleText().equals("32")) {
-                  r2.setStyle("-fx-background-color:#ff7a65");
-              } else if (r2.getAccessibleText().equals("64")) {
-                  r2.setStyle("-fx-background-color:#eb4e2f");
-              } else if (r2.getAccessibleText().equals("128")) {
-                  r2.setStyle("-fx-background-color:#f0df73");
-              } else if (r2.getAccessibleText().equals("256")) {
-                  r2.setStyle("-fx-background-color:#f0df48");
-              } else if (r2.getAccessibleText().equals("512")) {
-                  r2.setStyle("-fx-background-color:#f2cf09");
-              } else if (r2.getAccessibleText().equals("1024")) {
-                  r2.setStyle("-fx-background-color:#ffee6b");
-              } else if (r2.getAccessibleText().equals("2048")) {
-                  r2.setStyle("-fx-background-color:#fff587");
-              }
-
          }
-//         while(full!=1){
-//             board.setOnKeyPressed(e -> {
-//                 if(e.getCode() == KeyCode.KP_LEFT) {
-//                     for(int row=4; row>=0; row--){
-//                         for(int column=4; column>=0; column--){
-//                             if(a[row][column]==0){
-//                                 a[row][column]=1;
-//                             }
-//                         }
-//                     }
-//                 }
-//                if(e.getCode() == KeyCode.KP_RIGHT){
-//                    for(int row=4; row>=0; row--){
-//                        for(int column=0; column<=4; column++){
-//                             if(a[row][column]==0){
-//                                 //move over cube
-//                             }
-//                        }
-//                    }
-//                }
-//                if(e.getCode() == KeyCode.KP_UP){
-//                    for(int row=4; row>=0; row--){
-//                        for(int column=0; column<=4; column++){
-//                             if(a[row][column]==0){
-//                                 //move over cube
-//                             }
-//                        }
-//                    }
-//                }
-//                if(e.getCode() == KeyCode.KP_DOWN) {
-//                    for(int row=4; row>=0; row--){
-//                        for(int column=0; column<=4; column++){
-//                             if(a[row][column]==0){
-//                                 //move over cube
-//                             }
-//                        }
-//                    }
-//                }
-//             });
-//         }
 
-//        while (board.getChildren().get(0) == null && board.getChildren().get(1) == null && board.getChildren().get(2)
-//                == null) {
-//            board.setOnKeyPressed(e -> {
-//                if (e.getCode() == KeyCode.RIGHT) {
-//                    if (board.getChildren().get(1) == null);{
-//
-//                    }
-//                }
-//            });
-//        }
+        board.setOnKeyPressed(e -> {
+
+            if (e.getCode() == RIGHT) {
+                for (int row = 0; row < 4; row++) {
+                    for (int c = 0; c < 4; c++) {
+                        if (a[row][c] != null) {
+                            board.getChildren().remove(a[row][c]);
+
+                            if (a[row][3] != null) {
+                                if (a[row][3] == a[row][c] && a[row][3] == makeS2()) {
+                                    StackPane s4 = makeS4();
+                                    board.add(s4, 3, row);
+                                }
+                            }
+                            else {
+                                board.add(a[row][c], 3, row);
+                                a[row][3] = a[row][c];
+                            }
+                        }
+                    }
+
+                }
+            }
+            if (e.getCode() == LEFT) {
+                for (int row = 0; row < 4; row++) {
+                    for (int c = 0; c < 4; c++) {
+                        if (a[row][c] != null) {
+                            board.getChildren().remove(a[row][c]);
+                            board.add(a[row][c], 0, row);
+                        }
+                    }
+
+                }
+            }
+
+        });
 
         background.setTop(topBar);
         background.setBottom(i);
@@ -280,5 +269,34 @@ public class Emma2048 extends Application {
         ps.setTitle("Emma2048");
         ps.setScene(scene);
         ps.show();
+    }
+    public StackPane makeS2() {
+        Rectangle r2 = new Rectangle();
+        r2.setArcHeight(10);
+        r2.setArcWidth(10);
+        r2.setFill(Color.web("#eee4da"));
+        r2.setHeight(79);
+        r2.setWidth(79);
+        Text t2 = new Text("2");
+        t2.setFont(Font.font ("Calibri", FontWeight.BOLD, 40));
+        t2.setFill(Color.web("#776e65"));
+        StackPane s2 = new StackPane();
+        s2.getChildren().addAll(r2, t2);
+        return s2;
+    }
+
+    public StackPane makeS4() {
+        Rectangle r4 = new Rectangle();
+        r4.setArcHeight(10);
+        r4.setArcWidth(10);
+        r4.setFill(Color.web("#eee4da"));
+        r4.setHeight(79);
+        r4.setWidth(79);
+        Text t4 = new Text("2");
+        t4.setFont(Font.font ("Calibri", FontWeight.BOLD, 40));
+        t4.setFill(Color.web("#776e65"));
+        StackPane s4 = new StackPane();
+        s4.getChildren().addAll(r4, t4);
+        return s4;
     }
 }
